12f7e117f5b0f0a6006af3ac277c73ded7d99195
Антон Лермонтов <lermontov.a.a@mail.ru>
2022-05-29 17:56:07 +0300

    Ji-touch-test

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+"use strict";gsap.timeline().from(".content_title",{duration:1,ease:"power4.in",opacity:0});
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+let timeLine = gsap.timeline();
+timeLine
+  .from('.content_title', {duration: 1 ,ease: "power4.in", opacity: 0 })
